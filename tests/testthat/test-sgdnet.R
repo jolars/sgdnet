4b1f98a27b6ce51197f1eff0b07be7570c81f4c5
Johan Larsson <johanlarsson@outlook.com>
2018-05-18 19:16:58 +0200

    Add a simply test to check the output from sgdnet

--- tests/testthat/test-sgdnet.R
+++ tests/testthat/test-sgdnet.R
@@ -1,6 +1,9 @@
-context("main model fitter")
+context("gaussian regression")
 
 test_that("FitModel returns correct output", {
-  fit <- FitModel()
-  expect_is(fit, "list")
+  x <- matrix(rnorm(30), 10, 3)
+  y <- as.matrix(rnorm(nrow(x)))
+
+  fit <- sgdnet(x, y)
+  expect_s3_class(fit, "sgdnet")
 })
