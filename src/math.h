3e2fc53a6c81c5a15f2c823d659050c8d08fef89
Johan Larsson <johanlarsson@outlook.com>
2018-05-18 19:12:48 +0200

    Provide LogSumExp and SoftThresholding
    * LogSumExp will be used for the multinomial
    family and is setup to avoid under- and
    overflows.
    * SoftThresholding is the proximal operator
    for l1-regularization.

---
+++ src/math.h
@@ -0,0 +1,45 @@
+// sgdnet: Penalized Generalized Linear Models with Stochastic Gradient Descent
+// Copyright (C) 2018  Johan Larsson
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef SGDNET_MATH_
+#define SGDNET_MATH_
+
+#include <RcppArmadillo.h>
+
+//' LogSumExp function
+//'
+//' @param x a vector in the log domain
+//'
+//' @return `log(sum(exp(x)))` while avoiding over/underflow.
+inline double LogSumExp(const arma::vec& x) {
+  double x_max = x.max();
+  return std::log(arma::accu(arma::exp(x - x_max))) + x_max;
+}
+
+//' Soft thresholding operator
+//'
+//' Solves \f$ \argmin_{x} 0.5||x - y||^{2} + \alpha ||x||_{1} \f$.
+//'
+//' @param x a double
+//' @param y a double
+//'
+//' @return
+inline double SoftThresholding(const double x,
+                               const double shrinkage) {
+  return std::max(x - shrinkage, 0.0) - std::max(-x - shrinkage, 0.0);
+}
+
+#endif // SGDNET_MATH_
