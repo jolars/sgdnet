91353dfa0362d64a18ed8f272deb661603c8cdf8
Johan Larsson <johanlarsson@outlook.com>
2018-05-20 11:39:52 +0200

    Complement tests with sparse feature input test

--- tests/testthat/test-gaussian.R
+++ tests/testthat/test-gaussian.R
@@ -1,9 +1,20 @@
 context("gaussian regression")
 
-test_that("FitModel returns correct output", {
+test_that("gaussian regression with dense features work", {
   x <- matrix(rnorm(30), 10, 3)
   y <- as.matrix(rnorm(nrow(x)))
 
   fit <- sgdnet(x, y)
+  expect_s3_class(fit, "sgdnet")
+})
+
+
+test_that("gaussian regression with sparse features work", {
+  set.seed(1)
+  x <- Matrix::rsparsematrix(10, 3, density = 0.5)
+  y <- as.matrix(rnorm(nrow(x)))
+
+  fit <- sgdnet(x, y, alpha = 0)
+
   expect_s3_class(fit, "sgdnet")
 })
