4c139a501804ec2744b8c43116944745e842662e
Johan Larsson <johanlarsson@outlook.com>
2018-05-23 14:10:42 +0200

    Expand tests
    Expand tests to now cover
    
    * Comparing theoretical ridge regression results withthose from sgdnet
    * Comparing results from OLS regression.
    * Comparing output from dense and sparse features.
    
    We also now use real data sets instead of only simulated data.

--- tests/testthat/test-gaussian.R
+++ tests/testthat/test-gaussian.R
@@ -1,20 +1,95 @@
 context("gaussian regression")
 
-test_that("gaussian regression with dense features work", {
-  x <- matrix(rnorm(30), 10, 3)
-  y <- as.matrix(rnorm(nrow(x)))
+test_that("gaussian regression with dense features work as expected", {
+  x <- iris[, 1:3]
+  y <- iris[, 4]
 
-  fit <- sgdnet(x, y)
-  expect_s3_class(fit, "sgdnet")
+  # Check that we get the expected class
+  sgd_fit <- sgdnet(x, y, alpha = 0)
+  expect_s3_class(sgd_fit, "sgdnet")
 })
 
 
-test_that("gaussian regression with sparse features work", {
-  x <- Matrix::rsparsematrix(10, 3, density = 0.5)
-  y <- as.matrix(rnorm(nrow(x)))
+test_that("gaussian regression with sparse and dense features work", {
+  n <- 100
+  d <- 3
 
-  fit <- sgdnet(x, y, alpha = 0)
+  x <- Matrix::rsparsematrix(n, d, density = 0.5)
+  y <- x[, 1]*runif(n, 0.1, 0.2) + x[, 2]*rnorm(n, 0.4)
 
-  expect_s3_class(fit, "sgdnet")
+  seed <- sample.int(100, 1)
+
+  set.seed(seed)
+  fit_sparse <- sgdnet(x, y, alpha = 0, intercept = FALSE)
+  set.seed(seed)
+  fit_dense <- sgdnet(as.matrix(x), y, alpha = 0, intercept = FALSE)
+
+  diff <- coef(fit_sparse)[-1]/coef(fit_dense)[-1]
+
+  expect_equal(diff, rep(1, length(diff)), tolerance = 0.001)
 })
 
+test_that("we can approximately reproduce the OLS solution", {
+  airquality <- na.omit(airquality)
+  x <- as.matrix(subset(airquality, select = c("Wind", "Temp")))
+  y <- airquality$Ozone
+
+  sgd_fit <- sgdnet(x, y, lambda = 0, maxit = 1000, thresh = 0.0001)
+  ols_fit <- lm(y ~ x)
+
+  expect_equivalent(coef(ols_fit), as.vector(coef(sgd_fit)),
+                    tolerance = 0.01)
+})
+
+test_that("all weights are zero when lambda > lambda_max", {
+  # Code for this test has been borrowed from
+  # https://stats.stackexchange.com/questions/166630/glmnet-compute-maximal-lambda-value
+
+  # TODO(jolars): current this test is useless since the lasso implementation
+  #               is defunct
+
+  set.seed(1)
+
+  n <- 500
+  p <- 3
+  b <- c(-5, 3, 2)
+
+  x <- scale(matrix(rnorm(p * n), nrow = n))
+  y <- rnorm(n, mean = x%*%b)
+
+  alpha <- 1
+
+  lambda_max <- max(abs(t(y - mean(y)*(1 - mean(y))) %*% x)) / (alpha*n)
+
+  fit <- sgdnet(x, y, alpha = 1, lambda = lambda_max, maxit = 1000,
+                thresh = 0.0001)
+
+  # fit <- sgdnet(x, y, alpha = 1, lambda = lambda_max *0.9, maxit = 1000,
+  #               thresh = 0.0001)
+
+  expect_equal(coef(fit)[-1], rep(0, length(coef(fit)) - 1))
+})
+
+test_that("we can approximate the closed form ridge regression solution", {
+  set.seed(1)
+
+  n <- 500
+  p <- 3
+  b <- c(-5, 3, 2)
+
+  x <- scale(matrix(rnorm(p * n), nrow = n))
+  y <- rnorm(n, mean = x%*%b)
+  lambda <- 0.01
+  sd_y <- sqrt(var(y)*(n - 1) / n)
+
+  beta_theoretical <- solve((crossprod(x) + lambda*diag(p))) %*% crossprod(x, y)
+  sgdnet_fit <- sgdnet(x, y,
+                       alpha = 0,
+                       lambda = sd_y*lambda/n,
+                       intercept = FALSE,
+                       thresh = 0.00001,
+                       maxit = 1000)
+
+  expect_equivalent(beta_theoretical, coef(sgdnet_fit)[-1],
+                    tolerance = 1e-3)
+})
