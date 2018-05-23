5c6a9d460928dd37c22f1e04c01a60cfbc4dbbb3
Johan Larsson <johanlarsson@outlook.com>
2018-05-23 15:08:03 +0200

    Provide package-level documentation with information on options

--- R/sgdnet_package.R
+++ R/sgdnet_package.R
@@ -1,3 +1,20 @@
 #' @useDynLib sgdnet, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 NULL
+
+#' Penalized Generalized Linear Models with Stochastic Gradient Descent
+#'
+#' @section Package options:
+#'
+#' Parts of the [sgdnet] API can be interfaced with via
+#' \code{\link{options}} as follows:
+#'
+#' \itemize{
+#'   \item \code{sgdnet.debug}: set to `TRUE` to enable debugging features,
+#'                              which will be accessible via a `'debug_info'`
+#'                              attribute to the `'sgdnet'` object
+#'                              that is returned by [sgdnet()].
+#' }
+#' @name sgdnet-package
+#' @docType package
+NULL
