b6f0f905363e4381bea9b3093cfc4f09ef532044
Johan Larsson <johanlarsson@outlook.com>
2018-05-28 12:08:01 +0200

    Suppress plotting output from tests using dont_plot() and check xvar argument in plotting

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,12 +1,20 @@
 context("plotting")
 
 test_that("we can produce plots for all out models", {
+  dont_plot <- function(x, ...) {
+    tmp <- tempfile()
+    grDevices::png(tmp)
+    p <- graphics::plot(x, ...)
+    grDevices::dev.off()
+    unlink(tmp)
+    invisible(p)
+  }
+
   y <- mtcars$mpg
   x <- subset(mtcars, select = -mpg)
 
   fit <- sgdnet(x, y)
 
-  plot <- plot(fit)
-
-  expect_is(plot, "trellis")
+  expect_is(dont_plot(fit, "norm"), "trellis")
+  expect_is(dont_plot(fit, "lambda"), "trellis")
 })
