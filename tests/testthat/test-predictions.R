f9f591f8ebe262c2acaba004c79de01b97fc9151
Johan Larsson <johanlarsson@outlook.com>
2018-05-30 20:32:52 +0200

    Add a few more tests for predict.sgdnet

--- tests/testthat/test-predictions.R
+++ tests/testthat/test-predictions.R
@@ -16,19 +16,30 @@ test_that("prediction for gaussian models peform as expected", {
   # test that these predictions are equal
   expect_equal(pred_link, pred_response, pred_manual)
 
+  # check that we get the expected nonzero indices
   pred_nonzero <- predict(fit, x, type = "nonzero")
   expect_is(pred_nonzero, "list")
   expect_length(pred_nonzero, ncol(coef(fit)))
   expect_equivalent(pred_nonzero[[1]], NULL)
+  expect_equal(predict(fit, x, s = max(fit$lambda), type = "nonzero")[[1]],
+               NULL)
+  expect_equal(predict(fit, x, s = 0, type = "nonzero")[[1]], seq_len(ncol(x)))
 
   pred_coefficients <- predict(fit, x, type = "coefficients")
   expect_equal(pred_coefficients, coef(fit))
 
   # check linear interpolation
-  pred_new <- predict(fit, x, s = 0.04, type = "coefficients")
-  pred_two <- predict(sgdnet(x, y, lambda = 0.04), type = "coefficients")
+  pred_old <- predict(fit, x, s = 0.04, type = "coefficients")
+  pred_new <- predict(sgdnet(x, y, lambda = 0.04), type = "coefficients")
+
+  expect_equivalent(pred_old, pred_new, tolerance = 0.01)
   expect_is(pred_new, "dgCMatrix")
-  expect_equivalent(pred_new, pred_two, tolerance = 0.1)
+
+  pred_one <- predict(fit, x, s = 0.03, type = "coefficients")
+  pred_two <- predict(fit, x, s = 0.05, type = "coefficients")
+
+  expect_true(all(abs(as.vector(pred_one)) > abs(as.vector(pred_old))))
+  expect_true(all(abs(as.vector(pred_two)) < abs(as.vector(pred_old))))
 })
 
 test_that("assertations for incorrect input throw errors", {
