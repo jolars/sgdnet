9229d3deed772b1e3cf0962d2c7b06e74cc641c8
Johan Larsson <johanlarsson@outlook.com>
2018-06-03 20:25:20 +0200

    Fix URL format in Mushrooms data set

--- R/data.R
+++ R/data.R
@@ -65,6 +65,6 @@
 #'   Knopf.
 #' @source <https://archive.ics.uci.edu/ml/datasets/mushroom>
 #' @source Dua, D. and Karra Taniskidou, E. (2017).
-#'   UCI Machine Learning Repository [http://archive.ics.uci.edu/ml]. Irvine,
+#'   UCI Machine Learning Repository <http://archive.ics.uci.edu/ml>. Irvine,
 #'   CA: University of California, School of Information and Computer Science.
 "mushrooms"
