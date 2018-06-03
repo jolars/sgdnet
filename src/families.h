605e25688b8ac52c3c1da238c5dfd9f80f7ac0c1
Johan Larsson <johanlarsson@outlook.com>
2018-06-03 20:28:16 +0200

    Add binomial family to sgdnet

--- src/families.h
+++ src/families.h
@@ -32,24 +32,39 @@ public:
   virtual arma::rowvec Gradient(const arma::rowvec& prediction,
                                 const arma::rowvec& y) = 0;
 
-  virtual arma::uword NClasses(const arma::mat& y) = 0;
-
-  virtual arma::vec StepSize(const double       max_squared_sum,
-                             const arma::vec&   alpha_scaled,
-                             const bool         fit_intercept,
-                             const arma::uword  n_samples) = 0;
-
   virtual void PreprocessResponse(arma::mat&    y,
                                   arma::rowvec& y_center,
                                   arma::rowvec& y_scale,
                                   const bool    fit_intercept) = 0;
 
-  virtual double Deviance(const arma::mat& y,
-                          const arma::mat& prediction) = 0;
+  virtual double NullDeviance(const arma::mat& y) = 0;
+
+  arma::vec StepSize(const double       max_squared_sum,
+                     const arma::vec&   alpha_scaled,
+                     const bool         fit_intercept,
+                     const arma::uword  n_samples) {
+    // Lipschitz constant approximation
+    arma::vec L = L_scaling*(max_squared_sum + fit_intercept) + alpha_scaled;
+    arma::vec mu_n = 2.0*n_samples*alpha_scaled;
+    return 1.0 / (2.0*L + arma::min(L, mu_n));
+  };
+
+  arma::uword GetNClasses() {
+    return n_classes;
+  }
+
+protected:
+  arma::uword n_classes;
+  double L_scaling;
 };
 
 class Gaussian : public Family {
 public:
+  Gaussian() {
+    n_classes = 1;
+    L_scaling = 1.0;
+  };
+
   virtual ~Gaussian() {};
 
   double Loss(const arma::mat& prediction,
@@ -60,20 +75,6 @@ public:
   arma::rowvec Gradient(const arma::rowvec& prediction,
                         const arma::rowvec& y) {
     return prediction - y;
-  };
-
-  arma::uword NClasses(const arma::mat& y) {
-    return 1;
-  };
-
-  arma::vec StepSize(const double       max_squared_sum,
-                     const arma::vec&   alpha_scaled,
-                     const bool         fit_intercept,
-                     const arma::uword  n_samples) {
-    // Lipschitz constant approximation
-    arma::vec L = (max_squared_sum + fit_intercept) + alpha_scaled;
-    arma::vec mu_n = 2.0*n_samples*alpha_scaled;
-    return 1.0 / (2.0*L + arma::min(L, mu_n));
   };
 
   void PreprocessResponse(arma::mat&    y,
@@ -96,10 +97,48 @@ public:
     }
   };
 
-  double Deviance(const arma::mat& prediction,
-                  const arma::mat& y) {
-    return 1.0 -
-      arma::accu(arma::square(prediction - y))/arma::accu(arma::square(y));
+  double NullDeviance(const arma::mat& y) {
+    arma::vec prediction(y.n_rows);
+    prediction.fill(arma::accu(y)/y.n_rows);
+    return 2.0 * Loss(prediction, y);
+  }
+};
+
+class Binomial : public Family {
+public:
+  Binomial() {
+    n_classes = 1;
+    L_scaling = 0.25;
+  };
+
+  virtual ~Binomial() {};
+
+  double Loss(const arma::mat& prediction,
+              const arma::mat& y) {
+    return -arma::accu(y%prediction
+                       - arma::trunc_log(1.0 + arma::trunc_exp(prediction)));
+  };
+
+  arma::rowvec Gradient(const arma::rowvec& prediction,
+                        const arma::rowvec& y) {
+    return 1.0 - y - 1.0/(1.0 + arma::trunc_exp(prediction));
+  };
+
+  void PreprocessResponse(arma::mat&    y,
+                          arma::rowvec& y_center,
+                          arma::rowvec& y_scale,
+                          const bool    fit_intercept) {
+    y_center.zeros();
+    y_scale.ones();
+  };
+
+  double NullDeviance(const arma::mat& y) {
+    // Fit an intercept-only model
+    double y_mean = arma::accu(y)/y.n_rows;
+    arma::vec prediction(y.n_rows);
+    prediction.fill(std::log(y_mean/(1.0 - y_mean)));
+
+    return 2.0 * Loss(prediction, y);
   }
 };
 
@@ -108,6 +147,8 @@ public:
   static std::unique_ptr<Family> NewFamily(const std::string& family_choice) {
     if (family_choice == "gaussian")
       return std::unique_ptr<Family>(new Gaussian());
+    else if (family_choice == "binomial")
+      return std::unique_ptr<Family>(new Binomial());
     return NULL;
   };
 };
