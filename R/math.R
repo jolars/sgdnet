392736e376445004cef4ee0b198a868fe80f36cb
Johan Larsson <johanlarsson@outlook.com>
2018-07-13 12:51:19 +0200

    Move softmax prediction to predict.sgdnet and drop temporary and unused code

--- R/math.R
+++ R/math.R
@@ -14,31 +14,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-softmax <- function(x) {
-  d <- dim(x)
-  nas <- apply(is.na(x), 1, any)
-  if (any(nas)) {
-    pclass <- rep(NA, d[1])
-    if (sum(nas) < d[1]) {
-      pclass2 <- softmax(x[!nas, ])
-      pclass[!nas] <- pclass2
-      if (is.factor(pclass2))
-        pclass <- factor(pclass, levels = seq(d[2]), labels = levels(pclass2))
-    }
-  } else {
-    maxdist <- x[, 1]
-    pclass <- rep(1, d[1])
-    for (i in seq(2, d[2])) {
-      l <- x[, i] > maxdist
-      pclass[l] <- i
-      maxdist[l] <- x[l, i]
-    }
-    dd <- dimnames(x)[[2]]
-    pclass <- if (is.null(dd) || !length(dd))
-      pclass
-    else
-      factor(pclass, levels = seq(d[2]), labels = dd)
-  }
-  pclass
-}
 
