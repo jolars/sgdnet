c175c2564fd28954300efd842c96583cf62942e1
Johan Larsson <johanlarsson@outlook.com>
2018-07-18 21:58:39 +0200

    Drop tolerance in linear interpolation test

--- tests/testextras.R
+++ tests/testextras.R
@@ -10,6 +10,6 @@ pred_exact <- predict(fit,
                       y = trees$Volume)
 pred_approx <- predict(fit, trees$Girth, s = 0.001)
 
-if (!isTRUE(all.equal(pred_exact, pred_approx, tolerance = 1e-9))) {
+if (!isTRUE(all.equal(pred_exact, pred_approx, tolerance = 1e-4))) {
   stop("linear interpolation does approximate the exact solution")
 }
