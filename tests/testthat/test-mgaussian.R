e7f8447a8f9da74a2d10647933b645a6b914c3d6
Johan Larsson <johanlarsson@outlook.com>
2018-08-01 08:13:08 +0200

    Test that multivariate gaussian can reproduce the ridge solution

---
+++ tests/testthat/test-mgaussian.R
@@ -0,0 +1,28 @@
+context("multivariate gaussian regression")
+
+test_that("we can approximate the closed form multivariate ridge regression solution", {
+  set.seed(1)
+
+  n <- 500
+  p <- 3
+  b <- cbind(c(-5, 3, 2), c(0, -5, 9))
+
+  x <- scale(matrix(rnorm(p * n), nrow = n))
+  E <- x %*% b
+  y <- cbind(rnorm(n, colMeans(E)[1]), rnorm(n, colMeans(E)[2]))
+  lambda <- 0.01
+  sd_y <- apply(y, 2, function(y) sqrt(var(y)*(n - 1) / n))
+
+  beta_theoretical <- solve((crossprod(x) + lambda*diag(p))) %*% crossprod(x, y)
+  sgdnet_fit <- sgdnet(x, y,
+                       family = "mgaussian",
+                       alpha = 0,
+                       lambda = sd_y[1]*lambda/n,
+                       intercept = FALSE,
+                       thresh = 1e-5,
+                       maxit = 1e4)
+
+  expect_equivalent(beta_theoretical,
+                    as.matrix(do.call(cbind, coef(sgdnet_fit))[-1, ]),
+                    tolerance = 1e-6)
+})
