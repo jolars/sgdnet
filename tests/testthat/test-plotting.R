aaf6fd379463ed42b99d9f1b64b77e809d37a629
Johan Larsson <johanlarsson@outlook.com>
2018-08-11 23:07:23 +0200

    Remove dont_plot from test-plotting.R now that it is in setup.R

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,15 +1,6 @@
 context("plotting")
 
 test_that("we can produce plots for all out models", {
-  dont_plot <- function(x, ...) {
-    tmp <- tempfile()
-    grDevices::png(tmp)
-    p <- graphics::plot(x, ...)
-    grDevices::dev.off()
-    unlink(tmp)
-    invisible(p)
-  }
-
   y <- mtcars$mpg
   x <- subset(mtcars, select = -mpg)
 
