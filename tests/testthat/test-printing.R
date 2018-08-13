8b49495047976df2e15815847a212a8c13988176
Johan Larsson <johanlarsson@outlook.com>
2018-08-13 08:15:48 +0200

    Add a rudimentary print.cv_sgdnet() function

--- tests/testthat/test-printing.R
+++ tests/testthat/test-printing.R
@@ -1,11 +1,5 @@
 context("printing")
 
-# Suppress printning
-dont_print <- function(x, ...) {
-  utils::capture.output(y <- print(x, ...))
-  invisible(y)
-}
-
 test_that("printing works for univariate families", {
   fit <- sgdnet(rnorm(10), rnorm(10))
   expect_silent(dont_print(fit))
@@ -14,4 +8,9 @@ test_that("printing works for univariate families", {
 test_that("printing works for multivariate families", {
   fit <- sgdnet(rnorm(100), rbinom(100, 3, 0.3), family = "multinomial")
   expect_silent(dont_print(fit))
+})
+
+test_that("printing cv_sgdnet objects works", {
+  cv_fit <- cv_sgdnet(rnorm(10), rnorm(10))
+  expect_silent(dont_print(cv_fit))
 })
