99005644c9a13da3615581c11a890ce0d089bd91
Daisy <qinchenglu@gmail.com>
2019-08-06 12:44:49 -0700

    modify test cases

--- tests/testthat/test-penalty.R
+++ tests/testthat/test-penalty.R
@@ -7,13 +7,13 @@ test_that("we expect lasso solution when the non-convexity parameter is large en
   x <- scale(as.matrix(subset(airquality, select = c("Wind", "Temp"))))
   y <- scale(airquality$Ozone)
 
-  sgd_fit <- sgdnet(x, y, alpha = 1, thresh=1e-5)
-  sgd_mcp <- sgdnet(x, y, non_convexity = 1e+10, lambda = sgd_fit$lambda, thresh=1e-5, penalty = "MCP")
-  sgd_scad <- sgdnet(x, y, non_convexity = 1e+10, lambda = sgd_fit$lambda,thresh=1e-5,  penalty = "SCAD")
+  sgd_fit <- sgdnet(x, y, alpha = 1, thresh=1e-9)
+  sgd_mcp <- sgdnet(x, y, non_convexity = 1e+10, lambda = sgd_fit$lambda, thresh=1e-9, penalty = "MCP")
+  sgd_scad <- sgdnet(x, y, non_convexity = 1e+10, lambda = sgd_fit$lambda,thresh=1e-9,  penalty = "SCAD")
 
   expect_equivalent(coef(sgd_fit), coef(sgd_mcp),
-                    tolerance = 0.001)
+                    tolerance = 1e-7)
 
   expect_equivalent(coef(sgd_fit), coef(sgd_scad),
-                    tolerance = 0.001)
+                    tolerance = 1e-7)
 })
